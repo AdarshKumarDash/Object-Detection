24a29c627ee63be834566d476bb015ec7343409c
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-06-17 19:29:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
+status = "";
+
 function setup() {
     canvas = createCanvas(750, 480);
     canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelloaded);
+    document.getElementById("status").innerHTML = "Detecting Objects";
+}
+
+function modelloaded() {
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, getResult);
+}
+
+function getResult(error, result) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(result);
+    }
 }
 
 function preload() {
@@ -12,9 +30,11 @@ function draw() {
     fill("red")
     strokeWeight(1);
     text("Dog", 100, 40);
+    text("Cat", 360, 40)
     textSize(25);
     noFill();
     stroke("red");
     strokeWeight(3);
-    rect(100, 50, 260 , 420)
+    rect(100, 50, 260 , 420);
+    rect(350, 50, 340 , 420)
 }
